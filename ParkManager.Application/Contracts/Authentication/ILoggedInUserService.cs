563c6cc1685df3563311f75ef5fddf866d543b08
Emem Isaac <me@ememisaac.com>
2024-06-12 00:08:12 +0100

    Will help with user identification

---
+++ ParkManager.Application/Contracts/Authentication/ILoggedInUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkManager.Application.Contracts.Authentication
+{
+    public interface ILoggedInUserService
+    {
+        public string UserId { get; }
+    }
+}
