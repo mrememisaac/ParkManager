fdf4501d7fc407a45e2d6f1f1eb187c768a10eed
Emem Isaac <me@ememisaac.com>
2024-06-13 13:23:11 +0100

    Adds support for client supplied id and data type correction

--- ParkManager.Application/Features/Vehicles/Commands/AddVehicle/AddVehicleCommand.cs
+++ ParkManager.Application/Features/Vehicles/Commands/AddVehicle/AddVehicleCommand.cs
@@ -5,6 +5,7 @@ namespace ParkManager.Application.Features.Vehicles.Commands.AddVehicle
 {
     public class AddVehicleCommand : IRequest<AddVehicleCommandResponse>
     {
+        public Guid Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public string Registration { get; set; }
--- ParkManager.Application/Features/Vehicles/Commands/UpdateVehicle/UpdateVehicleCommand.cs
+++ ParkManager.Application/Features/Vehicles/Commands/UpdateVehicle/UpdateVehicleCommand.cs
@@ -5,6 +5,7 @@ namespace ParkManager.Application.Features.Vehicles.Commands.UpdateVehicle
 {
     public class UpdateVehicleCommand : IRequest<UpdateVehicleCommandResponse>
     {
+        public Guid Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public string Registration { get; set; }
