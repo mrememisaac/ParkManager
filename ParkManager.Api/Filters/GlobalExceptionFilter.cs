56cbdd37e92e8893ede7ce2396f807f9ef6e1933
Emem Isaac <me@ememisaac.com>
2024-06-15 22:14:12 +0100

    Adds a global exception handler

---
+++ ParkManager.Api/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParkManager.Api.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            // Handle the exception and set the result
+            context.Result = new ObjectResult("An error occurred")
+            {
+                StatusCode = 500
+            };
+            context.ExceptionHandled = true;
+        }
+    }
+
+
+}
