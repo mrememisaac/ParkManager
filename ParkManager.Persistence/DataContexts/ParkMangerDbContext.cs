ac10f48cefeca521406e85259a632d16140f8769
Emem Isaac <me@ememisaac.com>
2024-06-18 09:54:01 +0100

    Adds additional entty sets

--- ParkManager.Persistence/DataContexts/ParkMangerDbContext.cs
+++ ParkManager.Persistence/DataContexts/ParkMangerDbContext.cs
@@ -19,6 +19,8 @@ namespace ParkManager.Persistence.DataContexts
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Driver> Drivers { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<Slot> Slots { get; set; }
+        public DbSet<Lane> Lanes { get; set; }
         public DbSet<Occasion> Occasions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
